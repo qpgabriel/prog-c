07130e62eb1cfdc3a75e71b2f760c2cee3178600
rintokazushi <rintokazushi@gmail.com>
2017-05-05 10:37:09 -0400

    aula1c

---
+++ presbatata.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main ()
+{
+    printf("Boa noite\n");
+    printf("Meu nome é Batata\n");
+    printf("Este eh meu segundo prrograma em C\n");
+    printf("Modifiquei meu programa\n");
+  return 0;
+}
